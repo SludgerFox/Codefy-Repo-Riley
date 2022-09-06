32d6c4011537c8d4b912a50c5266d2050c9f980c
SludgerFox <73816137+SludgerFox@users.noreply.github.com>
2022-09-06 14:43:24 -0500

    Added Practice for Day 3

---
+++ Day 3/Day 3 Exercise/Index.js
@@ -0,0 +1,10 @@
+function printString(a = 1, b = 2){
+    if( a = b){
+        console.log(`A equals ${b}`);
+    }
+    else{
+        console.log(`A does not equal ${b}`);
+    }
+}
+
+printString();
